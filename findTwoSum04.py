1183f6de33c0442986ab372b41ed85607a7ee157
Davy Bendezu <djbendezu@gmail.com>
2020-09-23 00:19:57 -0500

    buscar en un array los números que sumen cierto valor, devuelve posiciones

---
+++ findTwoSum04.py
@@ -0,0 +1,23 @@
+def dosSuman(arr,target):
+    max=0
+    min=0
+    sum=0
+    for i in range(len(arr)):
+        if(arr[i]<target):
+            if(arr[i]>=min and arr[i]<=max):
+                for j in range(i):
+                    if((arr[j]+arr[i])==target):
+                        print(target, "["+str(j)+" "+str(i)+"]")
+                        return sum
+                    sum+=1
+            aux=target-arr[i]
+            if(aux>max):
+                max=aux
+            elif(aux<min):
+                min=aux
+        
+        sum+=1
+    print(target, "don't exist solution")
+    return sum
+
+dosSuman([2,7,11,15],9)
